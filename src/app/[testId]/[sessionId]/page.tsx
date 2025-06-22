b43ec6555c120b828e70b2cdd996011c7bc6025c
sohaekim <sohaekim1@gmail.com>
2025-06-22 15:23:04 +0900

    fix: vercel build error

--- src/app/[testId]/[sessionId]/page.tsx
+++ src/app/[testId]/[sessionId]/page.tsx
@@ -1,10 +1,5 @@
 import QuizResultComponent from "@/components/quiz/QuizResult";
-import { supabaseQuizService } from "@/services/supabaseQuizService";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
+// import { QueryClient } from "@tanstack/react-query";
 
 type Params = Promise<{
   testId: string;
@@ -14,22 +9,23 @@ type Params = Promise<{
 export default async function Page({ params }: { params: Params }) {
   const { testId, sessionId } = await params;
 
-  const queryClient = new QueryClient();
+  // const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: ["quizResult", testId, sessionId],
-    queryFn: () => supabaseQuizService.getQuizResult(testId, sessionId),
-  });
-  await queryClient.prefetchQuery({
-    queryKey: ["quiz", testId],
-    queryFn: () => supabaseQuizService.getQuiz(testId),
-  });
+  // await queryClient.prefetchQuery({
+  //   queryKey: ["quizResult", testId, sessionId],
+  //   queryFn: () => supabaseQuizService.getQuizResult(testId, sessionId),
+  // });
+  // await queryClient.prefetchQuery({
+  //   queryKey: ["quiz", testId],
+  //   queryFn: () => supabaseQuizService.getQuiz(testId),
+  // });
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      {/* <Suspense fallback={<div>loading</div>}> */}
-      <QuizResultComponent testId={testId} sessionId={sessionId} />
-      {/* </Suspense> */}
-    </HydrationBoundary>
+    <QuizResultComponent testId={testId} sessionId={sessionId} />
+    // // <HydrationBoundary state={dehydrate(queryClient)}>
+    //   {/* <Suspense fallback={<div>loading</div>}> */}
+
+    //   {/* </Suspense> */}
+    // {/* </HydrationBoundary> */}
   );
 }
