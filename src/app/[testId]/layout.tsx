5517b0896a753f30ca5c1ff17d08847e48743c38
sohaekim <sohaekim1@gmail.com>
2025-06-25 17:43:01 +0900

    update: quiz page layout에 vh 추가

--- src/app/[testId]/layout.tsx
+++ src/app/[testId]/layout.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    const setVH = () => {
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    };
+    setVH(); // 초기실행
+    window.addEventListener("resize", () => setVH());
+
+    return () => window.removeEventListener("resize", setVH);
+  }, []);
+
   return (
     <div className="bg-gray-50">
       <main
